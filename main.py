68ff61b77a5652742d7de8a1710d2970874e7754
rodrPS <rodrigospinheiro22@gmail.com>
2024-07-16 21:03:46 -0300

    Versão 1.1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
 import argparse
-import numpy as np
+
 from ultralytics import YOLO
 import supervision as sv
+import numpy as np
+
 
 ZONE_POLYGON = np.array([
     [0, 0],
@@ -11,11 +13,18 @@ ZONE_POLYGON = np.array([
     [0, 1]
 ])
 
+
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="YOLOv8 live")
-    parser.add_argument("--webcam-resolution", default=[1280, 720], nargs=2, type=int)
+    parser.add_argument(
+        "--webcam-resolution", 
+        default=[1280, 720], 
+        nargs=2, 
+        type=int
+    )
     args = parser.parse_args()
     return args
+
 
 def main():
     args = parse_arguments()
@@ -36,7 +45,7 @@ def main():
     zone_polygon = (ZONE_POLYGON * np.array(args.webcam_resolution)).astype(int)
     zone = sv.PolygonZone(polygon=zone_polygon, frame_resolution_wh=tuple(args.webcam_resolution))
     zone_annotator = sv.PolygonZoneAnnotator(
-        zone=zone,
+        zone=zone, 
         color=sv.Color.red(),
         thickness=2,
         text_thickness=4,
@@ -48,28 +57,29 @@ def main():
 
         result = model(frame, agnostic_nms=True)[0]
         detections = sv.Detections.from_yolov8(result)
-
-        selected_classes = [model.names.index("person")]
-        detections = detections[np.isin(detections.class_id, selected_classes)]
-
         labels = [
-            f"{model.model.names[class_id]} {confidence:.2f}"
+            f"{model.model.names[class_id]} {confidence:0.2f}"
             for _, confidence, class_id, _
             in detections
         ]
         frame = box_annotator.annotate(
-            scene=frame,
-            detections=detections,
+            scene=frame, 
+            detections=detections, 
             labels=labels
         )
 
         zone.trigger(detections=detections)
-        frame = zone_annotator.annotate(scene=frame)
-
+        frame = zone_annotator.annotate(scene=frame)      
+        
         cv2.imshow("yolov8", frame)
+
+        num_pessoas = sum(1 for _, _, class_id, _ in detections if class_id == 0)
+        print(f"Quantidade de pessoas na imagem: {num_pessoas}")
+
 
         if (cv2.waitKey(30) == 27):
             break
+
 
 if __name__ == "__main__":
     main()
